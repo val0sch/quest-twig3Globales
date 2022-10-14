10a7f4064d3ece1e615057b77eba3dde30e857c8
Valerie <valerie.ielsch@gmail.com>
2022-10-14 15:26:29 +0200

    challenge ok

--- config/twig.php
+++ config/twig.php
@@ -5,3 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
 $twig = new Twig\Environment($loader, ['debug' => true]);
 $twig->addExtension(new Twig\Extension\DebugExtension());
+
+//On ajoute la globale author à l'initialisation de l'objet Twig
+$contactEmail = 'tomatoketchup@mayo.fr';
+$twig->addGlobal('contactEmail', $contactEmail);
